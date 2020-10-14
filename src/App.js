d3b3e2b4266cabbb214b76ea03b7491141fc9f04
Dylan <dylanw@uci.edu>
2020-10-14 12:00:44 -0700

    set up react router and set up links to home and about page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
+import Alert from "./components/layout/Alert";
+import About from "./components/pages/About";
 import axios from "axios";
 import "./App.css";
 
@@ -9,6 +12,7 @@ class App extends Component {
   state = {
     users: [],
     loading: false,
+    alert: null
   };
   // async componentDidMount() {
   //   this.setState({ loading: true });
@@ -33,20 +37,40 @@ class App extends Component {
 
   clearUsers = () => this.setState({users:[], loading:false})
 
+  // set alert 
+  setAlert= (msg, type) => {
+    this.setState({alert:{msg,type}})
+    setTimeout(()=> this.setState({alert:null}),5000)
+  }
+
   render() {
 
     const {users, loading} = this.state;
 
     return (
+      <Router>
       <div className="App">
         <Navbar />
         <div className="container">
+          <Alert alert={this.state.alert}/>
+          <Switch>
+            <Route exact path="/" render={props=>(
+              <Fragment>
           <Search searchUsers={this.searchUsers} 
-          clearUsers={this.clearUsers} 
-          showClear={users.length > 0 ? true : false}/>
+            clearUsers={this.clearUsers} 
+            showClear={users.length > 0 ? true : false}
+            setAlert = {this.setAlert}
+          />
           <Users loading={loading} users={users} />
+              </Fragment>
+            )}/>
+
+            <Route exact path="/about" component={About} />
+          </Switch>
+
         </div>
       </div>
+      </Router>
     );
   }
 }
